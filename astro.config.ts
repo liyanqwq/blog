f393a56b132b2300012951b9ce76dcaeb36a2a21
Li Yan <liyanqwq@gmail.com>
2026-01-14 17:30:18 +0800

    New Version - Init Commit

---
+++ astro.config.ts
@@ -0,0 +1,88 @@
+import { defineConfig, envField, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import {
+  transformerNotationDiff,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+} from "@shikijs/transformers";
+import { transformerFileName } from "./src/utils/transformers/fileName";
+import { SITE } from "./src/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: SITE.website,
+  integrations: [
+    sitemap({
+      filter: page => SITE.showArchives || !page.endsWith("/archives"),
+    }),
+  ],
+  markdown: {
+    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    shikiConfig: {
+      // For more themes, visit https://shiki.style/themes
+      themes: { light: "min-light", dark: "night-owl" },
+      defaultColor: false,
+      wrap: false,
+      transformers: [
+        transformerFileName({ style: "v2", hideDot: false }),
+        transformerNotationHighlight(),
+        transformerNotationWordHighlight(),
+        transformerNotationDiff({ matchAlgorithm: "v3" }),
+      ],
+    },
+  },
+  i18n: {
+    locales: ["zh-HK", "en"],
+    defaultLocale: "zh-HK",
+    routing: {
+        prefixDefaultLocale: false
+    }
+  },
+  vite: {
+    // eslint-disable-next-line
+    // @ts-ignore
+    // This will be fixed in Astro 6 with Vite 7 support
+    // See: https://github.com/withastro/astro/issues/14030
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  image: {
+    responsiveStyles: true,
+    layout: "constrained",
+  },
+  env: {
+    schema: {
+      PUBLIC_GOOGLE_SITE_VERIFICATION: envField.string({
+        access: "public",
+        context: "client",
+        optional: true,
+      }),
+    },
+  },
+  experimental: {
+    preserveScriptOrder: true,
+    fonts: [
+      {
+        name: "Google Sans Code",
+        cssVariable: "--font-google-sans-code",
+        provider: fontProviders.google(),
+        fallbacks: ["monospace"],
+        weights: [300, 400, 500, 600, 700],
+        styles: ["normal", "italic"],
+      },
+      {
+        name: "LXGW WenKai TC",
+        cssVariable: "--font-lxgw-wenkai-tc",
+        provider: fontProviders.google(),
+        fallbacks: ["Google Sans Code"],
+        weights: [300, 400, 700],
+        styles: ["normal"],
+      }
+    ],
+  },
+});
---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://blog.liyan.moe/", // replace this with your deployed domain
+  author: "Li Yan",
+  profile: "https://liyan.moe/",
+  desc: "數字内外，煮碼論道。 Brewing code within and beyond the digital great wall.",
+  title: "對岸茶館 Cyber Teahouse - Li Yan's Blog",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: false,
+  showBackButton: false, // show back button in post detail
+  editPost: {
+    enabled: false,
+    text: "Edit page",
+    url: "https://github.com/satnaing/astro-paper/edit/main/",
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "zh-HK", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Hong_Kong", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
